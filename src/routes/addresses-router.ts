40bac8c21d89ac40b7959749441c9f71bd432a5f
Oreda-Z <shtucer31@gmail.com>
2022-06-13 09:13:53 +0300

    feat: add Data-Access-Layer for products-repository

--- src/routes/addresses-router.ts
+++ src/routes/addresses-router.ts
@@ -1,4 +1,5 @@
 import {Request, Response, Router} from "express"
+import {soundsRouter} from "./sounds-router";
 
 const addresses = [{id: 1, value: 'Nezalejnasti 12'}, {id: 2, value: 'Selickaga 11'}]
 
@@ -11,6 +12,38 @@ addressesRouter.get('/:id', (req: Request, res: Response) => {
     const id = +req.params.id
     const address = addresses.find(a => a.id === id)
     if(address) {
+        res.send(address)
+    } else {
+        res.sendStatus(404)
+    }
+})
+addressesRouter.delete('/:id', (req: Request, res: Response) => {
+    const id = +req.params.id
+    const index = addresses.findIndex(s => s.id === id)
+    if(index === -1) {
+        res.sendStatus(404)
+    } else {
+        res.send(addresses.splice(index, 1))
+    }
+})
+addressesRouter.post('/', (req: Request, res: Response) => {
+    const address = {
+        id: +(new Date()),
+        value: req.body.title
+    }
+    addresses.push(address)
+    res.status(201).send(address)
+})
+addressesRouter.put('/:id', (req: Request, res: Response) => {
+    // const id = +req.params.id
+    // if(id) {
+    //     res.send(sounds.map(s => s.id === id ? {...s, title: req.body.title} : s))
+    // } else {
+    //     res.status(404)
+    // }
+    let address = addresses.find(s => s.id === +req.params.id)
+    if(address) {
+        address.value = req.body.value
         res.send(address)
     } else {
         res.sendStatus(404)
--- src/routes/products-router.ts
+++ src/routes/products-router.ts
@@ -1,19 +1,46 @@
 import {Request, Response, Router} from 'express'
-
-const products = [{id: 1, title: 'tomato'}, {id: 2, title: 'orange'}]
+import {productsRepository} from "../repositories/products-repository";
 
 export const productsRouter = Router({})
 
 productsRouter.get('/', (req: Request, res: Response) => {
-    res.send(products)
+    const foundProducts = productsRepository.findProducts(req.query.title?.toString())
+    res.send(foundProducts)
+})
+productsRouter.get('/:id', (req: Request, res: Response) => {
+    let product = productsRepository.findProductById(+req.params.id)
+    if (product) {
+        res.send(product)
+    } else {
+        res.sendStatus(404)
+    }
+})
+productsRouter.post('/', (req: Request, res: Response) => {
+    const newProduct = productsRepository.creatProduct(req.body.title)
+    res.sendStatus(201).send(newProduct)
 })
 productsRouter.get('/:productTitle', (req: Request, res: Response) => {
-    const productName = req.params.productTitle
-    const product = products.find(p => p.title === productName)
-    if(!product) {
+    const product = productsRepository.findProductByTitle(req.body.title)
+    if (!product) {
         res.sendStatus(404)
     } else {
         res.send(product)
     }
-    res.send(product)
+})
+productsRouter.put('/:id', (req: Request, res: Response) => {
+    const isUpdated = productsRepository.updateProduct(+req.params.id, req.body.title)
+    if (isUpdated) {
+        const product = productsRepository.findProductById(+req.params.id)
+        res.send(product)
+    } else {
+        res.sendStatus(404)
+    }
+})
+productsRouter.delete('/:id', (req: Request, res: Response) => {
+    const isDeleted = productsRepository.deleteProduct(+req.params.id)
+    if(isDeleted) {
+        res.sendStatus(204)
+    } else {
+        res.sendStatus(404)
+    }
 })
